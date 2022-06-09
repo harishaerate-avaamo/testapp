1590d4aadab56a9ab22dca31770e82d043eb3d19
harishaerate-avaamo <103748114+harishaerate-avaamo@users.noreply.github.com>
2022-06-09 16:45:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const PORT = 3003;
+const PORT = 3000;
 const moment = require('moment')
 // const ngrok = require('ngrok');
 app.use(express.json());
